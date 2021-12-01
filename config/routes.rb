ebd21db8d8259b661a5c1138f7e0b841f785d761
nidhi-tntra <nidhi.patel@tntra.io>
2021-12-01 19:28:58 +0530

    Intital Commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'items_imports/new'
+
+  get 'items_imports/create'
+  
+  resources :items
+
+  resources :items_imports, only: [:new, :create]
+end
---
+++ test/helpers/items_imports_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class ItemsImportsHelperTest < ActionView::TestCase
+end
